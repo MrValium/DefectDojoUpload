87f39873186a9840a7db5825f39fa0cf1b924862
Giovanni Bernardo <80125934+MrValium@users.noreply.github.com>
2022-07-16 13:27:41 +0200

    Update my-hooks.py

--- zapFP/my-hooks.py
+++ zapFP/my-hooks.py
@@ -3,8 +3,6 @@ def zap_get_alerts(zap, baseurl, denylist, out_of_scope_dict):
     pg = 5000
     false_positives = [(10096, 'http://example.com'),(10021,'http://192.168.49.2:31190/swagger-ui/swagger-ui.css'),(10038,'http://192.168.49.2:31190/'),(10038,'http://192.168.49.2:31190')]
     alerts = zap.core.alerts(baseurl=baseurl, start=st, count=pg)
-    logging.debug('AAAAAAAAAAAAA')
-    logging.debug(alerts)
     while len(alerts) > 0:
         for alert in alerts:
             alert_id = alert.get('id')
