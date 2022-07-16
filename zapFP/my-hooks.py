94352b9dddbf07289e34914aa3450685cb9365a9
Giovanni Bernardo <80125934+MrValium@users.noreply.github.com>
2022-07-16 12:46:38 +0200

    aa

--- zapFP/my-hooks.py
+++ zapFP/my-hooks.py
@@ -5,13 +5,13 @@ def zap_get_alerts(zap, baseurl, denylist, out_of_scope_dict):
     alerts = zap.core.alerts(baseurl=baseurl, start=st, count=pg)
     while len(alerts) > 0:
         for alert in alerts:
-          alert_id = alert.get('id')
-          url = alert.get('url')
-          plugin_id = alert.get('pluginId')
+            alert_id = alert.get('id')
+            url = alert.get('url')
+            plugin_id = alert.get('pluginId')
             
             for fp in false_positives:
-              if plugin_id == fp[0] and url == fp[1]:
-                zap.alert.update_alerts_confidence(alert_id, '0')
-         st += pg
-         alerts = zap.core.alerts(start=st, count=pg)
+                if plugin_id == fp[0] and url == fp[1]:
+                    zap.alert.update_alerts_confidence(alert_id, '0')
+        st += pg
+        alerts = zap.core.alerts(start=st, count=pg)
     return alerts
