b7f862959e63e17c6533b72101f67ed601534857
Giovanni Bernardo <80125934+MrValium@users.noreply.github.com>
2022-07-16 12:57:52 +0200

    Update my-hooks.py

--- zapFP/my-hooks.py
+++ zapFP/my-hooks.py
@@ -12,6 +12,6 @@ def zap_get_alerts(zap, baseurl, denylist, out_of_scope_dict):
             for fp in false_positives:
                 if plugin_id == fp[0] and url == fp[1]:
                     zap.alert.update_alerts_confidence(alert_id, '0')
-        st += pg
-        alerts = zap.core.alerts(start=st, count=pg)
+            st += pg
+            alerts = zap.core.alerts(start=st, count=pg)
     return alerts
