66b71426239e6afaa7cd02adfad036878096ed5c
Giovanni Bernardo <80125934+MrValium@users.noreply.github.com>
2022-07-16 16:56:20 +0200

    qqq

--- zapFP/my-hooks.py
+++ zapFP/my-hooks.py
@@ -1,18 +1,20 @@
 def zap_get_alerts(zap, baseurl, denylist, out_of_scope_dict):
     st = 0
     pg = 5000
-    false_positives = [(10096, 'http://example.com'),(10021,'http://192.168.49.2:31190/swagger-ui/swagger-ui.css'),(10038,'http://192.168.49.2:31190/'),(10038,'http://192.168.49.2:31190')]
+    false_positives = [[10096, 'http://example.com'],[10021,'http://192.168.49.2:31190/swagger-ui/swagger-ui.css'],[10038,'http://192.168.49.2:31190/'],[10038,'http://192.168.49.2:31190']]
     alerts = zap.core.alerts(baseurl=baseurl, start=st, count=pg)
+    alert_dict = {}
+    alert_count = 0
     while len(alerts) > 0:
+        logging.debug('Reading ' + str(pg) + ' alerts from ' + str(st))
+        alert_count += len(alerts)
         for alert in alerts:
             alert_id = alert.get('id')
             url = alert.get('url')
             plugin_id = alert.get('pluginId')
-            
             for fp in false_positives:
-                #if true: 
-                #plugin_id == fp[0] and url == fp[1]:
-                    zap.alert.update_alerts_confidence(alert_id, '0')
+                if plugin_id == fp[0] and url == fp[1]:
+                    zap.alert.update_alerts_confidence(alert_id, 0)
         st += pg
         alerts = zap.core.alerts(start=st, count=pg)
     return alerts
